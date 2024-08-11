7dff76fdb35894d772b26dbb82056a95c3dea837
ajmal a <apmajmal52@gmail.com>
2024-08-11 12:37:52 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Welcome from './components/Welcome';
 import Navbar from './components/Navbar'; // Import Navbar
 import EmailForm from './components/EmailForm';
 import OTPForm from './components/OTPForm';
-import Welcome from './components/Welcome';
+
 
 function App() {
   const [email, setEmail] = useState('');
@@ -13,9 +14,10 @@ function App() {
       <Navbar /> {/* Add Navbar */}
       <div className="container mx-auto p-4">
         <Routes>
+        <Route path="/welcome" element={<Welcome />} />
           <Route path="/" element={<EmailForm onOtpSent={setEmail} />} />
           <Route path="/verify-otp" element={<OTPForm email={email} />} />
-          <Route path="/welcome" element={<Welcome />} />
+          
         </Routes>
       </div>
     </Router>
